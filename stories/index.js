7745ab156bcfbe5f561d5469f16a3b249b2df0fe
Siu <siuc@protonmail.com>
2021-12-07 17:29:38 -0500

    add storyvook for confirm.js

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -139,4 +140,5 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
-  .add("Show", () => <Show student='Lydia Miller-Jones' interviewer='Sylvia Palmer' onEdit={action("onEdit")} onDelete={action("onDelete")} />);
+  .add("Show", () => <Show student='Lydia Miller-Jones' interviewer='Sylvia Palmer' onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+  .add("Confirm", () => <Confirm message='Delete the appointment?' onConfirm={action("onConfirm")} onCancel={action("onCancel")} />);
