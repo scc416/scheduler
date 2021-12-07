200ec398215eff798c88a469d96b80ae38908ea9
Siu <siuc@protonmail.com>
2021-12-06 19:23:10 -0500

    implement Button with classNames(package)"

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,14 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
   const { children, confirm, danger, onClick, disabled } = props;
 
-  let buttonClass = "button";
-
-  if (confirm) {
-    buttonClass += " button--confirm";
-  } else if (danger) {
-    buttonClass += " button--danger";
-  }
+  const buttonClass = classNames("button", {
+    "button--confirm": confirm,
+    "button--danger": danger,
+  });
 
   return (
     <button className={buttonClass} onClick={onClick} disabled={disabled}>
