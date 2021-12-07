47dc860dd8b5b263bdf8e45e702a8cce424ea4ed
Siu <siuc@protonmail.com>
2021-12-06 21:45:29 -0500

    implement DayList

---
+++ src/components/DayList.js
@@ -0,0 +1,22 @@
+import React, { setState } from "react";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  const { days, day, setDay } = props;
+  const list = days.map((dayInfo) => {
+    return (
+      <DayListItem
+        key={dayInfo.id}
+        name={dayInfo.name}
+        spots={day.spots}
+        selected={dayInfo.name === day}
+        setDay={setDay}
+      />
+    );
+  });
+  return (
+    <ul>
+      {list}
+    </ul>
+  );
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,22 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
 
 export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props;
+  const classes = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0,
+  });
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => setDay(name)} className={classes}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">
+        {spots === 0 && <>{'no spots remaining'}</>}
+        {spots === 1 && <>{'1 spot remaining'}</>}
+        {spots > 1 && <>{`${spots} spots remaining`}</>}
+      </h3>
     </li>
   );
 }
