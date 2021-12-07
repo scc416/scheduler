125e9953d94c23e8699e512dd9b7be72487acaa4
Siu <siuc@protonmail.com>
2021-12-07 17:14:27 -0500

    implement Show.js and its storybook

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "../src/components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -135,6 +136,7 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }],
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time='12pm'/>)
-  .add("Header", () => <Header time='12pm'/> )
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/> );
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show student='Lydia Miller-Jones' interviewer='Sylvia Palmer' onEdit={action("onEdit")} onDelete={action("onDelete")} />);
