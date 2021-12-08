d05fb93bfdbf49bb51f24236bce4de6575384e65
Siu <siuc@protonmail.com>
2021-12-08 18:45:58 -0500

    Add an empty back() function to the Hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
   const transition = setMode;
 
-  return { mode, transition };
+  const back = () => {};
+
+  return { mode, transition, back };
 }
