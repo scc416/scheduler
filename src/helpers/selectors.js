a56baefa71c87b73b0bd435ad7a58ba56f05611a
Siu <siuc@protonmail.com>
2021-12-08 16:44:52 -0500

    implement function: getInterview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,10 +9,15 @@ const getAppointmentId = (days, dayToBeFound) => {
 export function getAppointmentsForDay(state, day) {
   const { days, appointments } = state;
   const appointmentId = getAppointmentId(days, day);
-  const appointmentArr = appointmentId.map(id => appointments[id]);
+  const appointmentArr = appointmentId.map((id) => appointments[id]);
   return appointmentArr;
 }
 
-export function getInterview(state, interviewer) {
-  
+export function getInterview(state, interview) {
+  if (!interview) return null;
+
+  const { interviewers } = state;
+  const { interviewer } = interview;
+  const interviewerInfo = interviewers[interviewer];
+  return { ...interview, interviewer: interviewerInfo };
 }
