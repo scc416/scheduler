110c0a49b108fd2a3799fe104a7ce77252959776
Siu <siuc@protonmail.com>
2021-12-08 15:51:06 -0500

    implement selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,14 @@
-const getAppointmentsForDay = (state, day) => {
-  
+const getAppointmentId = (days, dayToBeFound) => {
+  for (const day of days) {
+    const { name, appointments } = day;
+    if (name === dayToBeFound) return appointments;
+  }
+  return [];
 };
+
+export function getAppointmentsForDay(state, day) {
+  const { days, appointments } = state;
+  const appointmentId = getAppointmentId(days, day);
+  const appointmentArr = appointmentId.map(id => appointments[id]);
+  return appointmentArr;
+}
