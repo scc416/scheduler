8d5163f7bfd44d0df5a1c34c40b7e272b6915aec
Siu <siuc@protonmail.com>
2021-12-07 20:06:56 -0500

    complete form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 const Appointment = (props) => {
-  const { time } = props;
-  return (<article className="appointment">
-    {!time&&'No appointments'}
-    {time&&`Appointment at ${time}`}</article>);
+  const { id, time, interview } = props;
+  console.log(interview);
+  return (
+    <article className="appointment">
+      <Header time={time} />
+      {interview && <Show {...interview} />}
+      {!interview && <Empty />}
+    </article>
+  );
 };
 
 export default Appointment;
