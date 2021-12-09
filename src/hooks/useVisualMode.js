02185d991c0f3eb35f004c9b5ff0aef4f988f3f4
Siu <siuc@protonmail.com>
2021-12-08 19:10:27 -0500

    Add the history array to the useVisualMode hook and update the back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,19 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = setMode;
+  const transition = (mode) => {
+    setMode(mode);
+    setHistory((prev) => [...prev, mode]);
+  };
 
-  const back = () => {};
+  const back = () => {
+    const historyLength = history.length;
+    const lastMode = history[historyLength - 2];
+    setMode(lastMode);
+    setHistory((prev) => prev.slice(0, historyLength - 1));
+  };
 
   return { mode, transition, back };
 }
