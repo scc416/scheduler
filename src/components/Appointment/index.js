b2c501b5b1360120aad7131761d4bf75fe24af7c
Siu <siuc@protonmail.com>
2021-12-09 00:43:55 +0000

    Update the onAdd function to pass to Empty component to the CREATE mode"

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,13 +5,19 @@ import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import useVisualMode from "hooks/useVisualMode";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 const Appointment = (props) => {
   const { time, interview } = props;
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Header time={time} />
       {interview && <Show {...interview} />}
-      {!interview && <Empty />}
+      {!interview && <Empty onAdd={transition(CREATE)} />}
     </article>
   );
 };
