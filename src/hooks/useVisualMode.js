15db56d7080590401ce702367ff32af72094ae7d
Siu <siuc@protonmail.com>
2021-12-08 19:26:40 -0500

    implement Transition with Replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,7 +4,12 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (mode) => {
+  const transition = (mode, replace) => {
+    if (replace) {
+      const historyLength = history.length;
+      setHistory((prev) => [...prev.slice(0, historyLength - 1), mode]);
+      return setMode(mode);
+    }
     setMode(mode);
     setHistory((prev) => [...prev, mode]);
   };
