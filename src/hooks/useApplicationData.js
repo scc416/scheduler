d59d298684cc491e10d42ce63c6f291424b2b267
Siu <siuc@protonmail.com>
2021-12-13 16:47:18 -0500

    remove unused function from useApplucationData.js

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,10 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import {
-  getAppointmentsForDay,
-  getInterview,
-  getInterviewersForDay,
-} from "../helpers/selectors";
 
 const useApplicationData = () => {
   const [state, setState] = useState({
