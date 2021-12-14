f0406bb83407035af895aa8dd1797aa02ce9272a
Siu <siuc@protonmail.com>
2021-12-14 16:42:23 -0500

    add 5th test for form

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -32,7 +32,6 @@ describe("Form", () => {
   });
 
   it("validates that the student name is not blank", () => {
-
     const onSave = jest.fn();
 
     const { getByText } = render(<Form {...{ onSave, interviewers }} />);
@@ -56,6 +55,21 @@ describe("Form", () => {
     fireEvent.click(saveButton);
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
+
+  it("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
+    );
+
+    const input = getByPlaceholderText("Enter Student Name");
+
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
