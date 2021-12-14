f9e247ad84b0f8a0aceb476e77ec11e7d3a2eccf
Siu <siuc@protonmail.com>
2021-12-14 17:27:21 -0500

    deconstruct interviewer at paramter

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Show = ({ student, interviewer, onEdit, onDelete }) => {
+const Show = ({ student, interviewer: { name }, onEdit, onDelete }) => {
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
         <h2 className="text--regular">{student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">{interviewer.name}</h3>
+          <h3 className="text--regular">{name}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
