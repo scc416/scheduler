c49ec909668bccf4779a9c1640b0317526396fab
Siu <siuc@protonmail.com>
2021-12-13 22:20:36 -0500

    use prop-types to test type of interviewer(for InterviewerList)

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 export default function InterviewerList({ interviewers, onChange, value } ) {
   const items = interviewers.map((interviewerInfo) => {
@@ -22,3 +23,7 @@ export default function InterviewerList({ interviewers, onChange, value } ) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
