4183c6351e95c8e618aecf8bb2bf9f0f75f65ff5
Siu <siuc@protonmail.com>
2021-12-14 22:01:57 -0500

    Add a new test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -12,10 +12,6 @@ import Application from "components/Application";
 afterEach(cleanup);
 
 describe("Application", () => {
-  it("renders without crashing", () => {
-    render(<Application />);
-  });
-
   it("changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
 
@@ -25,4 +21,6 @@ describe("Application", () => {
 
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {});
 });
