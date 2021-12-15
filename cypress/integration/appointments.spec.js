1e2f22c11bbe107bc972755967baf42639c7739d
Siu <siuc@protonmail.com>
2021-12-15 15:29:55 -0500

    Add the commands that verify that we show the student and interviewer names within and element that has the ".appointment__card--show" class

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,10 +1,12 @@
 describe("Appointment", () => {
   it("should book an interview", () => {
+    cy.request('get', '/api/debug/reset');
     cy.visit("/");
     cy.contains("Monday");
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=input]").type("Lydia Miller-Jones");
     cy.get(`[alt="Sylvia Palmer"]`).click();
-    //.contains('form');
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones", "Sylvia Palmer");
   });
 });
