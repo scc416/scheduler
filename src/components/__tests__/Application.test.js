fdb2f8851f339997a844dc0614c09955f572f4d0
Siu <siuc@protonmail.com>
2021-12-14 21:36:50 -0500

    change test to async

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -11,14 +16,13 @@ describe("Application", () => {
     render(<Application />);
   });
 
-  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  it("changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
-  
-    return waitForElement(() => getByText("Monday")).then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });
+
+    await waitForElement(() => getByText("Monday"));
+
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
-  
-  
 });
