8e1c350f9c264ac23e384e0d8c65ddbbcbd3c02b
Siu <siuc@protonmail.com>
2021-12-15 15:32:16 -0500

    Refactor the test to separate the common test commands from the ones specific to the "should book an interview"

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,12 +1,21 @@
 describe("Appointment", () => {
-  it("should book an interview", () => {
-    cy.request('get', '/api/debug/reset');
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+
     cy.visit("/");
+
     cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=input]").type("Lydia Miller-Jones");
     cy.get(`[alt="Sylvia Palmer"]`).click();
     cy.contains("Save").click();
-    cy.contains(".appointment__card--show", "Lydia Miller-Jones", "Sylvia Palmer");
+    cy.contains(
+      ".appointment__card--show",
+      "Lydia Miller-Jones",
+      "Sylvia Palmer"
+    );
   });
 });
