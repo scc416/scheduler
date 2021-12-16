2c668e08bc57f5b6e09ac69382386788f5e4b1dd
Siu <siuc@protonmail.com>
2021-12-15 20:18:51 -0500

    Render the application and store the container value returned by render

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,14 +9,22 @@ import {
 
 import Application from "components/Application";
 
-afterEach(cleanup);
-
 describe("Application", () => {
-  const { getByText } = render(<Application />);
+  afterEach(cleanup);
 
   it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
     await waitForElement(() => getByText("Monday"));
 
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container, getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"));
+    console.log(container);
     fireEvent.click(getByText("Tuesday"));
 
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
